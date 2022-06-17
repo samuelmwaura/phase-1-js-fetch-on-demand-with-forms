6f4a2a4b3aa16e29b44a53794d8a24c62eaf5181
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-06-17 23:19:09 +0300

    commit tree 4f292ceb948e29b7c4ace4a619f80e7cb15272c8

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 const init = () => {
-  
+  document.querySelector('form').addEventListener('submit',(event)=>{
+  event.preventDefault();
+  const testInput=document.getElementById('searchByID').value
+  fetch(`http://localhost:3000/movies/${testInput}`).then(response=>response.json()).then(data=>{
+      document.querySelector('section#movieDetails h4').textContent = data.title;
+      document.querySelector('section#movieDetails p').textContent = data.summary;  // Grabbing by css selectors
+
+  }); //The restful conventions really saved a lof of writing
+
+  })
 }
+
 
 document.addEventListener('DOMContentLoaded', init);
